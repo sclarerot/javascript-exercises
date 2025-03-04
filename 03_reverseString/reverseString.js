f2cffb217d8ce5350c356ca298cf12ac2ea2686a
sclarerot <sclarerot@proton.me>
2025-03-04 06:01:37 -0500

    Solve reverseString exercise
    I solved the exercise where you have to write a function that reverses a string. I just used JavaScript's built-in methods. Very simple.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let stringSplit = string.split("");
+  let arrayReversed = stringSplit.reverse("");
+  let stringReversed = arrayReversed.join("");
 
+if(string == "") {
+    return ""
+} else
+    return stringReversed;
 };
 
 // Do not edit below this line
