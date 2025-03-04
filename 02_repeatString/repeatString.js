514f681103f8c0eb411a59a6edd211ffedca08eb
sclarerot <sclarerot@proton.me>
2025-03-04 04:22:41 -0500

    Solve repeatString exercise.
    I finished up the first real exercise. It was actually a lot of fun figuring out how to pass the tests and fixing bugs. I wrote a detailed explanation of my code with comments.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    // First, I define a variable that is equal to the string argument.
+    let stringRepeater = string
+    /* Afterwards, I run two tests.
+    One to check if the number is less than zero, and if it is, "ERROR" is returned.
+    And second, one to check if the number is zero, which returns an empty string */
+    if(num < 0) {return "ERROR"}
+    if(num == 0) {return ""}
+    /* If both checks are false, a loop is run which repeatedly adds the string to itself 'num' amount of times.
+    This loop works by adding the stringRepeater variable to the string argument until num reaches one. */
+        while (num > 1) {
+          string = string + stringRepeater;
+          num--;
+    }
+    // Finally, the string is returned with the loop's alterations intact.
+    return string;
 };
 
 // Do not edit below this line
