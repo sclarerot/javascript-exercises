8518f93b69898ec0b8d8d0e2e5bce4180dac3fc1
sclarerot <sclarerot@proton.me>
2025-03-05 03:02:19 -0500

    Solve fourth exercise
    I solved the removeFromArray exercise using the built-in filter function, since it seemed to be the simplest and most concise way to deal with it.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(itemArray, ...removeItems) {
+  return itemArray.filter(item => !removeItems.includes(item))
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 2, 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
